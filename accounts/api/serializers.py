91f8a926a296e86fd50f361d7e22457bcd04c113
Tianhao Wang <tvw5292@gmail.com>
2021-09-12 11:46:50 -0400

    Comment Model API: Create

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -18,6 +18,10 @@ class UserSerializerForFriendships(UserSerializerForTweets):
     pass
 
 
+class UserSerializerForComments(UserSerializerForTweets):
+    pass
+
+
 class SignupSerializer(serializers.ModelSerializer):
     username = serializers.CharField(max_length=20, min_length=6)
     password = serializers.CharField(max_length=20, min_length=6)
--- twitter/urls.py
+++ twitter/urls.py
@@ -19,6 +19,7 @@ from accounts.api.views import UserViewSet, AccountViewSet
 from tweets.api.views import TweetViewsSet
 from friendships.api.views import FriendshipViewSet
 from newsfeeds.api.views import NewsFeedViewSet
+from comments.api.views import CommentViewSet
 
 router = routers.DefaultRouter()
 router.register(r'api/users', UserViewSet)
@@ -26,6 +27,7 @@ router.register(r'api/accounts', AccountViewSet, basename='accounts')
 router.register(r'api/tweets', TweetViewsSet, basename='tweets')
 router.register(r'api/friendships', FriendshipViewSet, basename='friendships')
 router.register(r'api/newsfeeds', NewsFeedViewSet, basename='newsfeeds')
+router.register(r'api/comments', CommentViewSet, basename='comments')
 
 
 urlpatterns = [
